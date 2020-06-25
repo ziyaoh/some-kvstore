29d5a76eab3ff3ccfab898a7dbf56951291ca232
ziyaoh <yaozi326@outlook.com>
2020-06-25 18:36:18 -0400

    some tests for client on acking finished request seq

--- replicationgroup/client_test.go
+++ replicationgroup/client_test.go
@@ -4,6 +4,7 @@ import (
 	"math/rand"
 	"testing"
 
+	"github.com/ziyaoh/some-kvstore/rpc"
 	"github.com/ziyaoh/some-kvstore/util"
 
 	"github.com/stretchr/testify/assert"
@@ -13,34 +14,57 @@ func TestClientWithNormalLeader(t *testing.T) {
 	util.SuppressLoggers()
 
 	leader := CreateDefaultMockRGNode()
+	leader.t = t
 	defer leader.GracefulExit()
+
 	addr := leader.Self.Addr
 	client, err := Connect(rand.Uint64(), addr)
 	assert.Nil(t, err, "Client connect to server fail")
 
+	leader.expected = &rpc.ClientRequest{
+		AckSeqs: []uint64{},
+	}
 	result, err := client.Get([]byte("key"))
 	assert.Nil(t, err, "client request fail")
 	assert.Equal(t, []byte("value"), result, "client Get result incorrect")
+
+	leader.expected = &rpc.ClientRequest{
+		AckSeqs: []uint64{0},
+	}
+	client.Get([]byte("key"))
 }
 
 func TestClientWithFailLeader(t *testing.T) {
 	util.SuppressLoggers()
 
-	leader := CreateFailMockRGNode()
-	defer leader.GracefulExit()
-	addr := leader.Self.Addr
-	client, err := Connect(rand.Uint64(), addr)
-	assert.Nil(t, err, "Client connect to server fail")
+	normalLeader := CreateDefaultMockRGNode()
+	defer normalLeader.GracefulExit()
 
+	addr := normalLeader.Self.Addr
+	client, err := Connect(rand.Uint64(), addr)
+	client.Get([]byte("key"))
+
+	failLeader := CreateFailMockRGNode()
+	failLeader.t = t
+	defer failLeader.GracefulExit()
+	failLeader.expected = &rpc.ClientRequest{
+		AckSeqs: []uint64{uint64(0)},
+	}
+
+	client.Leader = failLeader.Self
 	result, err := client.Get([]byte("key"))
 	assert.NotNil(t, err, "expect Get to error")
 	assert.Nil(t, result, "expect result to be empty")
+
+	// make sure client doesn't miss a to-be-ack seq in case request fail
+	client.Get([]byte("key"))
 }
 
 func TestClientWithFollower(t *testing.T) {
 	util.SuppressLoggers()
 
 	leader := CreateDefaultMockRGNode()
+	leader.t = t
 	defer leader.GracefulExit()
 	follower := CreateFollowerMockRGNode(leader.Self)
 	defer follower.GracefulExit()
@@ -52,6 +76,12 @@ func TestClientWithFollower(t *testing.T) {
 	result, err := client.Get([]byte("key"))
 	assert.Nil(t, err, "client request fail")
 	assert.Equal(t, []byte("value"), result, "client Get result incorrect")
+
+	leader.expected = &rpc.ClientRequest{
+		AckSeqs: []uint64{0},
+	}
+	client.Leader = follower.Self
+	client.Get([]byte("key"))
 }
 
 func TestClientWithStartingNode(t *testing.T) {
